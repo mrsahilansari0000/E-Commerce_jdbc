aa49898e57f4a43519ac5d484b7713e91132192b
mrsahilansari0000 <mrsahilansari2003@gmail.com>
2026-07-15 19:01:52 +0530

    Customer Queries

---
+++ src/main/java/com/ecommerce/constants/CustomerQueries.java
@@ -0,0 +1,14 @@
+
+package com.ecommerce.constants;
+
+public class CustomerQueries {
+	
+	private CustomerQueries() {
+		
+	}
+
+	public String userSignUpQuery = "INSERT INTO User(user_id, username, email, password, created_at, role) values(?,?,?,?,?,?)";
+
+	public String userLoginQuery = "SELECT * FROM User";
+
+}
